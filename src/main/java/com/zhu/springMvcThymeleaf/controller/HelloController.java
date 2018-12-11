76a218fa7e5724b37dd968182da9fbdf571acd22
LY <LY@DESKTOP-N150RQM>
2018-12-11 16:31:08 +0800

    Initial commit

---
+++ src/main/java/com/zhu/springMvcThymeleaf/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.zhu.springMvcThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("context","hello world");
+        return "hello";
+    }
+}
