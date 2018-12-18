affab494207d52743b1e75ecfec4afefcaed35fe
LY <LY@DESKTOP-N150RQM>
2018-12-18 16:02:36 +0800

    集成mybatis

--- src/main/java/com/zhu/springMvcThymeleaf/controller/HelloController.java
+++ src/main/java/com/zhu/springMvcThymeleaf/controller/HelloController.java
@@ -1,17 +1,26 @@
 package com.zhu.springMvcThymeleaf.controller;
 
+import com.zhu.springMvcThymeleaf.domain.User;
+import com.zhu.springMvcThymeleaf.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @Autowired
+    private UserMapper userMapper;
+    @RequestMapping("/")
     public String hello(Model model){
-        model.addAttribute("context","hello world");
+        List<User> users = userMapper.selectAll();
+        System.out.println(users);
+        model.addAttribute("users",users);
         return "hello";
     }
 }
